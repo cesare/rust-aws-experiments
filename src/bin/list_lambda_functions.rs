110be16be206804bc50f11d980279c19af90d065
SAWADA Tadashi <cesare@mayverse.jp>
2022-05-08 19:15:20 +0900

    Fix client initialization

--- src/bin/list_lambda_functions.rs
+++ src/bin/list_lambda_functions.rs
@@ -2,7 +2,8 @@ use aws_sdk_lambda::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::from_env();
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
     let response = client.list_functions().send().await?;
     println!("{:?}", response);
     Ok(())
